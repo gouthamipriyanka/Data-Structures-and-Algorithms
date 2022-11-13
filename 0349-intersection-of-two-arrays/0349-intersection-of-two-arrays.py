0697182688f4a567a8228822f78406cdef0c31cc
Gouthami <64356674+gouthami-tom@users.noreply.github.com>
2022-11-13 18:47:15 -0500

    Time: 80 ms (28.05%), Space: 13.6 MB (59.57%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -5,20 +5,32 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        l1 = len(nums1)
-        l2 = len(nums2)
+        # l1 = len(nums1)
+        # l2 = len(nums2)
         result = []
-        if l1 < l2:
-            for i in range(l1):
+        # if l1 < l2:
+        #     for i in range(l1):
+        #         if nums1[i] in nums2:
+        #             if nums1[i] not in result:
+        #                 result.append(nums1[i])
+        #     return result
+        # else:
+        #     for i in range(l2):
+        #         if nums2[i] in nums1:
+        #             if nums2[i] not in result:
+        #                 result.append(nums2[i])
+        #     return result
+        
+        for i in range(len(nums1)):
                 if nums1[i] in nums2:
                     if nums1[i] not in result:
-                        result.append(nums1[i])
-            return result
-        else:
-            for i in range(l2):
-                if nums2[i] in nums1:
-                    if nums2[i] not in result:
-                        result.append(nums2[i])
-            return result
-                
+                        result.append(nums1[i])        
+        return result
+    #Time Complexity:
+    """
+    for in operation: eg: for x in s -------O(1)
+    n = l1 if l1 < l2 else n = l2.
+    Overall TC = O(n**2) 
+    SC = O(n)
+    """
                     
